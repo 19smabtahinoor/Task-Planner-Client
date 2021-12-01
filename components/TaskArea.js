28dda295a685905c7f009414add65f3fba445a8f
S.M.ABTAHI NOOR <abtahinoorkabid@gmail.com>
2021-12-01 17:46:46 +0600

    Error Solved

--- components/TaskArea.js
+++ components/TaskArea.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const TaskArea = ({value}) => {
+    // const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
     return (
         <div className="">
             <h1 className="text-3xl font-semibold text-gray-700 date-font">{new Date(value).toLocaleDateString(undefined, options)}</h1>
+            
         </div>
     )
 }
